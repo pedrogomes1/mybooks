4b939358e1edced11b2db6448b24689cc68da6aa
Pedro <pedro-gomes17@hotmail.com>
2022-06-09 08:49:02 -0300

    feat: create a new Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import { Heart } from "phosphor-react";
+import logo from "../assets/logo.svg";
+
+import styles from "./Header.module.css";
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.content}>
+        <img
+          src={logo}
+          alt="Logo da aplicação contendo símbolo de chaves da cor roxa, e com o nome MyBooks no centro e da cor cinza claro"
+        />
+        <a href="#" className={styles.favoriteLink}>
+          <Heart size={24} />
+          Favoritos
+        </a>
+      </div>
+    </header>
+  );
+}
