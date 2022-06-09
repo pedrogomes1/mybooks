c3e1026ebdf65496b6767e4863c135c4fa0224d7
Pedro <pedro-gomes17@hotmail.com>
2022-06-09 09:24:32 -0300

    improvement: render new text with book suggestions

--- src/components/InputSearch/index.tsx
+++ src/components/InputSearch/index.tsx
@@ -3,9 +3,12 @@ import styles from "./InputSearch.module.css";
 
 export function InputSearch() {
   return (
-    <div className={styles.containerSearch}>
-      <input type="text" placeholder="Digite um livro para a busca" />
-      <MagnifyingGlass size={32} color="#8d8d99" />
-    </div>
+    <>
+      <div className={styles.containerSearch}>
+        <input type="text" placeholder="Digite um livro para a busca" />
+        <MagnifyingGlass size={32} color="#8d8d99" />
+      </div>
+      <i className={styles.suggestionsBooks}>(Ex: React, Javascript, Java)</i>
+    </>
   );
 }
