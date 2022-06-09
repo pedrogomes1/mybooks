a3facd8fa74839d4d8ce9672e24ece667c9e1472
Pedro <pedro-gomes17@hotmail.com>
2022-06-09 09:29:55 -0300

    improvement: reverse search icon order

--- src/components/InputSearch/index.tsx
+++ src/components/InputSearch/index.tsx
@@ -5,8 +5,12 @@ export function InputSearch() {
   return (
     <>
       <div className={styles.containerSearch}>
+        <MagnifyingGlass
+          className={styles.iconSearch}
+          size={32}
+          color="#8d8d99"
+        />
         <input type="text" placeholder="Digite um livro para a busca" />
-        <MagnifyingGlass size={32} color="#8d8d99" />
       </div>
       <i className={styles.suggestionsBooks}>(Ex: React, Javascript, Java)</i>
     </>
