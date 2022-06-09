0b06a1d3fd9631df8e19441c6c7a825f473001b4
Pedro <pedro-gomes17@hotmail.com>
2022-06-09 20:42:39 -0300

    feat: added request google api books and render dynamic book card data

--- src/components/BookCard/index.tsx
+++ src/components/BookCard/index.tsx
@@ -3,10 +3,20 @@ import { ChipCategories } from "../ChipCategories";
 
 import styles from "./BookCard.module.css";
 
-export function BookCard() {
+import { BookProps } from "../Books";
+
+interface BookCardProps {
+  book: BookProps;
+  onToggleBookDialogDetail: () => void;
+}
+
+export function BookCard({ book, onToggleBookDialogDetail }: BookCardProps) {
   return (
     <div className={styles.container}>
-      <img src={"https://github.com/pedrogomes1.png"} alt="Card" />
+      <img
+        src={book.imageLinks.thumbnail}
+        alt="Imagem representativa do livro"
+      />
       <div className={styles.content}>
         <ul className={styles.categoryChipList}>
           <li>
@@ -20,19 +30,20 @@ export function BookCard() {
           </button>
         </ul>
 
-        <h3>React.Js Essentials</h3>
-        <p className={styles.description}>
-          A fast-paced guide to designing and building scalable and maintainable
-          web apps with React.jsAbout This Book• Build maintainable and
-          performant user interfaces for your web applications using React.js•
-          Create reusable React.js components to save time and effort in
-          maintaining your user interfaces• Learn how to build a ready-to-deploy
+        <h3 data-testid="book-title">{book.title}</h3>
+        <p data-testid="book-description" className={styles.description}>
+          {book.description}
         </p>
         <div className={styles.containerPublishDate}>
           <h4>Publicado em:</h4>
-          <time>2021-01-05</time>
+          <time data-testid="book-publishedAt">{book.publishedDate}</time>
         </div>
-        <button className={styles.detailsButton}>Detalhes</button>
+        <button
+          onClick={onToggleBookDialogDetail}
+          className={styles.detailsButton}
+        >
+          Detalhes
+        </button>
       </div>
     </div>
   );
--- src/components/Books/index.tsx
+++ src/components/Books/index.tsx
@@ -1,17 +1,107 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+import { DialogBookDetail } from "../DialogBookDetail";
 import { BookCard } from "../BookCard";
 import { InputSearch } from "../InputSearch";
 
 import styles from "./Books.module.css";
 
+const URL_GET_BOOKS = `https://www.googleapis.com/books/v1/volumes?q=react&key=${
+  import.meta.env.VITE_API_KEY
+}`;
+
+export interface BookProps {
+  id: string;
+  title: string;
+  subtitle: string;
+  authors: string[];
+  publisher: string;
+  publishedDate: string;
+  description: string;
+  previewLink: string;
+  imageLinks: {
+    thumbnail: string;
+  };
+  categories: string[];
+}
+export interface IRequestBookProps {
+  items: {
+    id: string;
+    volumeInfo: BookProps;
+  }[];
+}
+
 export function Books() {
+  const [bookNameSearch, setBookNameSearch] = useState("");
+  const [isOpenBookDialogDetail, setIsOpenBookDialogDetail] = useState(true);
+  const [books, setBooks] = useState<BookProps[]>([]);
+
+  useEffect(() => {
+    async function fetchBooks() {
+      try {
+        const { data } = await axios.get<IRequestBookProps>(URL_GET_BOOKS);
+        const booksData = data.items.map(({ id, volumeInfo }) => {
+          const {
+            title,
+            subtitle,
+            authors,
+            categories,
+            description,
+            imageLinks,
+            previewLink,
+            publishedDate,
+            publisher,
+          } = volumeInfo;
+          return {
+            id,
+            title,
+            subtitle,
+            categories,
+            authors,
+            publisher,
+            publishedDate: new Date(publishedDate).toLocaleDateString("pt-br", {
+              timeZone: "UTC",
+            }),
+            description,
+            previewLink,
+            imageLinks,
+          };
+        });
+        setBooks(booksData);
+      } catch (error) {
+        alert(error);
+      }
+    }
+    fetchBooks();
+  }, []);
+
+  const handleToggleBookDialogDetail = () => {
+    setIsOpenBookDialogDetail((prev) => !prev);
+  };
+
   return (
     <main className={styles.container}>
       <InputSearch />
-      <ul className={styles.booksList}>
-        <li>
-          <BookCard />
-        </li>
-      </ul>
+      {!!books.length && (
+        <ul className={styles.booksList}>
+          {books.map((book) => (
+            <li key={book.id}>
+              <BookCard
+                book={book}
+                onToggleBookDialogDetail={handleToggleBookDialogDetail}
+              />
+            </li>
+          ))}
+        </ul>
+      )}
+
+      {isOpenBookDialogDetail && (
+        <DialogBookDetail
+          isOpen={isOpenBookDialogDetail}
+          onClose={handleToggleBookDialogDetail}
+        />
+      )}
     </main>
   );
 }
