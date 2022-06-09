78e057c454378f042e615f9d9ce01054656d1c8a
Pedro <pedro-gomes17@hotmail.com>
2022-06-09 12:35:43 -0300

    feat: create a ChipCategories component

---
+++ src/components/ChipCategories/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./ChipCategories.module.css";
+
+export function ChipCategories() {
+  return (
+    <div className={styles.container} title="Computer">
+      <span>Computer</span>
+    </div>
+  );
+}
