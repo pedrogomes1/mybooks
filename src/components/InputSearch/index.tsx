eb62d5c159e41b0d89d0caff20e6f17e6d150391
Pedro <pedro-gomes17@hotmail.com>
2022-06-09 09:08:28 -0300

    feat: create new InputSearch component

---
+++ src/components/InputSearch/index.tsx
@@ -0,0 +1,11 @@
+import { MagnifyingGlass } from "phosphor-react";
+import styles from "./InputSearch.module.css";
+
+export function InputSearch() {
+  return (
+    <div className={styles.containerSearch}>
+      <input type="text" placeholder="Digite um livro para a busca" />
+      <MagnifyingGlass size={32} color="#8d8d99" />
+    </div>
+  );
+}
