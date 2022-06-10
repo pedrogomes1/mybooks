347bb7326f900c38848ac50d377e1684aafbb1c8
Pedro <pedro-gomes17@hotmail.com>
2022-06-10 15:24:25 -0300

    improvements: small ajustments to home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,8 +5,12 @@ import { Header } from "../../components/Header";
 import { BooksList } from "../../components/BooksList";
 import { InputSearch } from "../../components/InputSearch";
 
-import styles from "./Home.module.css";
 import { useBooks } from "../../hooks/useBooks";
+import { RequestStatus } from "../../types/status";
+
+import styles from "./Home.module.css";
+
+const { error, loading, empty } = RequestStatus;
 
 export function Home() {
   const [bookNameSearch, setBookNameSearch] = useState("react");
@@ -26,15 +30,15 @@ export function Home() {
       <Header />
       <main className={styles.container}>
         <InputSearch onChange={handleSearchBook} />
-        {status === "loading" ? (
-          <div className={styles.feedback}>
+        {status === loading ? (
+          <div data-testid="spinner" className={styles.spinner}>
             <BeatSpinnerLoader color="#996DFF" />
           </div>
-        ) : status === "error" ? (
+        ) : status === error ? (
           <h3 className={styles.feedback}>
             Erro ao carregar os livros. Por favor, tente novamente mais tarde.
           </h3>
-        ) : status === "empty" ? (
+        ) : status === empty ? (
           <h3 className={styles.feedback}>Nenhum livro encontrado.</h3>
         ) : (
           <BooksList
