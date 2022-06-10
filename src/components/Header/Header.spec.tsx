cbe67bb503cadffe6e32abe24b5f126ab080a04e
Pedro <pedro-gomes17@hotmail.com>
2022-06-10 00:59:35 -0300

    refactor: adjustments Header component tests

--- src/components/Header/Header.spec.tsx
+++ src/components/Header/Header.spec.tsx
@@ -1,20 +1,33 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { Header } from ".";
+
+const mockedUsedNavigate = jest.fn();
+jest.mock("react-router-dom", () => ({
+  useNavigate: () => mockedUsedNavigate,
+}));
 
 describe("Header component", () => {
   beforeEach(() => {
     render(<Header />);
   });
 
-  it("should render image app logo", () => {
-    expect(
-      screen.getByRole("img", {
-        name: "Logo da aplicação contendo símbolo de chaves da cor roxa, e com o nome MyBooks no centro e da cor cinza claro",
-      })
-    ).toBeInTheDocument();
+  it("should render elements", () => {
+    const imageLogo = screen.getByRole("img", {
+      name: "Logo da aplicação contendo símbolo de chaves da cor roxa, e com o nome MyBooks no centro e da cor cinza claro",
+    });
+    const favoriteButton = screen.getByRole("button", { name: "Favoritos" });
+
+    expect(imageLogo).toBeInTheDocument();
+    expect(favoriteButton).toBeInTheDocument();
   });
 
-  it("should render image button favorite link", () => {
-    expect(screen.getByRole("link", { name: "Favoritos" })).toBeInTheDocument();
+  it("should redirect to my favorites page when click favorites button", async () => {
+    const user = userEvent.setup();
+    const favoriteButton = screen.getByRole("button", { name: "Favoritos" });
+
+    await user.click(favoriteButton);
+
+    expect(mockedUsedNavigate).toHaveBeenCalledWith("/favorites");
   });
 });
