604674a6895f0bce55412b52c97cbd319e2b26ca
Pedro <pedro-gomes17@hotmail.com>
2022-06-09 21:56:34 -0300

    feat: create new Home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import { Books } from "../../components/Books";
+import { Header } from "../../components/Header";
+
+export function Home() {
+  return (
+    <>
+      <Header />
+      <Books />
+    </>
+  );
+}
