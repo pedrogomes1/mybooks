02acea46922fc4a663e82b26159b905fedb8189a
Pedro <pedro-gomes17@hotmail.com>
2022-06-09 23:04:01 -0300

    improvement: added redirect to favorite page by button header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import { Heart } from "phosphor-react";
+import { Link } from "react-router-dom";
 import logo from "../../assets/logo.svg";
 
 import styles from "./Header.module.css";
@@ -11,10 +12,10 @@ export function Header() {
           src={logo}
           alt="Logo da aplicação contendo símbolo de chaves da cor roxa, e com o nome MyBooks no centro e da cor cinza claro"
         />
-        <a href="#" className={styles.favoriteLink}>
+        <Link to="favorites" className={styles.favoriteLink}>
           <Heart size={24} />
           Favoritos
-        </a>
+        </Link>
       </div>
     </header>
   );
