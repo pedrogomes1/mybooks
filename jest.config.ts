c16d3f89f474af1be15f3cd0d123bcf5937b7ea6
Pedro <pedro-gomes17@hotmail.com>
2022-06-10 17:22:56 -0300

    config: added needed configs to jest config file

--- jest.config.ts
+++ jest.config.ts
@@ -4,8 +4,15 @@ export default {
     "^.+\\.tsx?$": "ts-jest",
   },
   moduleNameMapper: {
-    "\\.(gif|ttf|eot|svg|png)$": "<rootDir>/test/__mocks__/fileMock.js",
+    "\\.(gif|ttf|eot|svg|png|jpg)$": "<rootDir>/test/__mocks__/fileMock.js",
     "\\.(css|less|sass|scss)$": "identity-obj-proxy",
   },
-  setupFilesAfterEnv: ["<rootDir>/src/jest-setup.ts"],
+  setupFilesAfterEnv: ["<rootDir>/src/tests/jest-setup.ts"],
+  globals: {
+    "ts-jest": {
+      isolatedModules: true,
+    },
+  },
+  collectCoverage: true,
+  collectCoverageFrom: ["src/components/**/*.ts(x)?", "src/pages/**/*.ts(x)?"],
 };
