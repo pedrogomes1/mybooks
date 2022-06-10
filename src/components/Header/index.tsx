2e667820e20dd57f4b271009370982579bec6498
Pedro <pedro-gomes17@hotmail.com>
2022-06-10 08:10:49 -0300

    improvement: replace link to button in favorite button on header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,16 @@
 import { Heart } from "phosphor-react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import logo from "../../assets/logo.svg";
 
 import styles from "./Header.module.css";
 
 export function Header() {
+  const navigate = useNavigate();
+
+  function handleNavigateToFavoritesPage() {
+    navigate("/favorites");
+  }
+
   return (
     <header className={styles.header}>
       <div className={styles.content}>
@@ -12,10 +18,13 @@ export function Header() {
           src={logo}
           alt="Logo da aplicação contendo símbolo de chaves da cor roxa, e com o nome MyBooks no centro e da cor cinza claro"
         />
-        <Link to="favorites" className={styles.favoriteLink}>
+        <button
+          className={styles.favoriteButton}
+          onClick={handleNavigateToFavoritesPage}
+        >
           <Heart size={24} />
           Favoritos
-        </Link>
+        </button>
       </div>
     </header>
   );
